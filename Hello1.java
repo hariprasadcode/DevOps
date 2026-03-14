f4dad14f4fe2f37379e72d178e217459168debd9
Hariprasadcode <hariprasadacharya993@gmail.com>
2026-03-14 22:57:49 +0530

    Adding File Hello1

---
+++ Hello1.java
@@ -0,0 +1,29 @@
+//just recallliing the conceptof java and trying to push the code in git .
+class Hello1{
+int a=10;
+//static block
+static{
+    System.out.println("hello developers , its the topics covered under java");
+}
+
+//non static block
+{
+    System.out.println("its non static block");
+}
+
+//constructor
+public Hello(){
+    System.out.println("its constructor");
+} 
+public void display(){
+    System.out.println("inside a method");
+}
+//main method
+public static void main(String[] args){
+    //craeting and objectand colling method
+    Hello h=new Hello();
+    h.display();
+}
+
+}
+
